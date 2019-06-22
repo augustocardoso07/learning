31de60139a755fbe068470f2b226883300b9de5a
Renan Augusto Cardoso <augustocardoso07@gmail.com>
2019-06-22 17:05:21 -0300

    86% on queues-submit

--- MOOCs/Algorithms Part I/week2/src/Permutation.java
+++ MOOCs/Algorithms Part I/week2/src/Permutation.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
@@ -10,10 +11,20 @@ public class Permutation {
 
         RandomizedQueue<String> q = new RandomizedQueue<String>();
 
-        boolean s = StdIn.isEmpty();
-        while (!s) {
-            q.enqueue(StdIn.readString());
-            s = StdIn.isEmpty();
+        int n = 0;
+        while (StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            n++;
+            if (q.size() == k) {
+                Double p = StdRandom.uniform();
+                if (p > 1/n) {
+                    q.dequeue();
+                    q.enqueue(word);
+                }
+            } else {
+                q.enqueue(word);
+            }
+
         }
 
         for (int i = 0; i < k; i++) {
